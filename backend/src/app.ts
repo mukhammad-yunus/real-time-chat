5047f1040cad2fc4fb971f22f9fac6551d2a17df
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-05-21 17:18:11 +0200

    add Request Body Size Limits

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 export const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "32kb" }));
 
-app.get('/', (_req: Request, res: Response)=>{
+app.get("/", (_req: Request, res: Response) => {
   res.json({
     ok: true,
-    message: "real-time-chat API"
-  })
-})
+    message: "real-time-chat API",
+  });
+});
