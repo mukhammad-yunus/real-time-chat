44f6c656eecb79df918933013506ab56d60cd4f3
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-05-21 10:18:41 +0200

    add express.json middleware for app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express";
 
 export const app = express();
 
+app.use(express.json());
+
 app.get('/', (_req: Request, res: Response)=>{
   res.json({
     ok: true,
