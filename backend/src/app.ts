a5c36c1b5c4da2d0bfcc1595712d8da16e0931d2
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-05-22 14:18:58 +0200

    add errorHandler and notFoundHandler middlewares for the app and made some cleanings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { errorHandler, notFoundHandler } from "./middleware/error-handler.js";
 
 export const app = express();
 
 app.use(express.json({ limit: "32kb" }));
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/", (_req, res) => {
   res.json({
     ok: true,
-    message: "real-time-chat API",
+    message: "Real-Time-Chat API",
   });
 });
+
+app.use(notFoundHandler);
+app.use(errorHandler);
