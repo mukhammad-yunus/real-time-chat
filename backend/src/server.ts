7edbcf227bf41b8292379c987ea13a4b12ab7039
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-05-23 18:10:10 +0200

    add Graceful Shutdown

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,3 +8,14 @@ const server = http.createServer(app)
 server.listen(PORT, ()=>{
     console.log(`Backend listening on http://localhost:${PORT}`);
 })
+
+function shutdown(signal: string) {
+  console.log(`${signal} received. Closing HTTP server.`);
+  server.close(() => {
+    console.log("HTTP server closed.");
+    process.exit(0);
+  });
+}
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
